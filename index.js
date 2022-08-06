c819fba5232bb1872d6e246a752f170de5cff0ee
crescendoCat <c.crescendo.cat@gmail.com>
2022-08-07 01:42:33 +0800

    feat: Add body checking and db file existence checking

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const express = require("express")
-
+const fs = require("fs")
 const PORT = 3000
 const app = express()
 
 app.use(express.json())
 
+const DB_PATH = "./listdb.json"
+
 app.get("/", (req, res) => {
-  return res.send("Hello");
+  let todolist = {listItems: []} 
+  if (fs.existsSync(DB_PATH)) {
+    todolist = JSON.parse(fs.readFileSync(DB_PATH));
+  } else {
+    fs.writeFileSync(DB_PATH, JSON.stringify(todolist))
+  }
+  return res.json(todolist);
 })
 
 app.post("/", (req, res) => {
-    let ret = {}
-    ret.bodyParam = req.body
-    ret.urlParam  = req.query
-    res.json(ret)
+  if(!req.body.listItems)  {
+    return res.status(422).json({
+      success: false,
+      message: "Body must contains listItems"
+    })
+  }
+  fs.writeFileSync(DB_PATH, JSON.stringify(req.body));
+  return res.json({"success": true})
 })
 
 app.listen(PORT, () => {
