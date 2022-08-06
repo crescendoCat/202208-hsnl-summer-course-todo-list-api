f6b0f2867b0748ce10f8913bf2636005f3d4400c
crescendoCat <c.crescendo.cat@gmail.com>
2022-08-07 01:52:14 +0800

    feat: add some comments and make route more RESTful

--- index.js
+++ index.js
@@ -4,21 +4,27 @@ const PORT = 3000
 const app = express()
 
 app.use(express.json())
-
+//統一定義DB檔案路徑
 const DB_PATH = "./listdb.json"
 
-app.get("/", (req, res) => {
-  let todolist = {listItems: []} 
+//取得listItems
+app.get("/list", (req, res) => {
+  let todolist = {listItems: []}
+  //檢查DB檔案是否存在
   if (fs.existsSync(DB_PATH)) {
     todolist = JSON.parse(fs.readFileSync(DB_PATH));
   } else {
+    //如果檔案不存在，創建檔案並寫入預設內容
     fs.writeFileSync(DB_PATH, JSON.stringify(todolist))
   }
   return res.json(todolist);
 })
 
-app.post("/", (req, res) => {
+//存入listItems
+app.post("/list", (req, res) => {
+  //檢查request body是否合法
   if(!req.body.listItems)  {
+    //
     return res.status(422).json({
       success: false,
       message: "Body must contains listItems"
